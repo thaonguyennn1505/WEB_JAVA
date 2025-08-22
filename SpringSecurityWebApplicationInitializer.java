90f9d614fa1d571065fc050d4a70eb49c857dcf7
thaonguyennn1505 <duongthihoa10004@gmail.com>
2025-08-22 19:41:29 +0700

    Delete SpringSecurityWebApplicationInitializer.java

--- SpringSecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package vn.shopttcn.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
